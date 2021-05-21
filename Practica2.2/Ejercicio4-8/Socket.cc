1da6d0c5a7ce5aed52dcf944b261ef1c42c25ff7
Juanma <cursoredes@localhost.localdomain>
2021-05-21 12:13:58 +0200

    inicializacion de sockets

--- Practica2.2/Ejercicio4-8/Socket.cc
+++ Practica2.2/Ejercicio4-8/Socket.cc
@@ -4,9 +4,38 @@
 #include "Socket.h"
 
 Socket::Socket(const char * address, const char * port):sd(-1)
-{
-    //Construir un socket de tipo AF_INET y SOCK_DGRAM usando getaddrinfo.
-    //Con el resultado inicializar los miembros sd, sa y sa_len de la clase
+{ 
+    //Construir un socket de tipo AF_INET y SOCK_DGRAM usando getaddrinfo   
+    struct addrinfo hintsInfo;
+    struct addrinfo * resInfo;
+
+    memset((void*) &hintsInfo, 0, sizeof(struct addrinfo));
+
+    hintsInfo.ai_family = AF_INET; //le decimos que tiene que ser ipv4
+    hintsInfo.ai_socktype = SOCK_DGRAM; //udp
+    
+    //hacemos la llamada por red
+    int info = getaddrinfo(address, port, &hintsInfo, &resInfo);
+
+    if(info != 0)
+    { //error
+        std::cout << "Se ha producido un error en el getaddrinfo\n";
+        return;
+    }
+
+    //Con el resultado inicializar sd
+    sd = socket(resInfo->ai_family, resInfo->ai_socktype, 0); //creamos un socket
+    if(sd == -1){
+        std::cout << "Se ha producido un error al crear el socket\n";
+        return;
+    }
+
+    //inicializamos sa y sa_len 
+    char host[NI_MAXHOST];
+    char service[NI_MAXSERV];
+
+    sa_len = sizeof(struct sockaddr);
+    getnameinfo(&sa, sa_len, host, NI_MAXHOST, service, NI_MAXSERV, NI_NUMERICHOST | NI_NUMERICSERV);
 }
 
 int Socket::recv(Serializable &obj, Socket * &sock)
